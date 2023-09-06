51cb8f9d197458811469f9cf7883e6524f263612
Junghwan <sl8792@naver.com>
2023-09-06 16:50:38 +0900

    Add checkMember method

---
+++ src/main/java/com/lib/ailibrary/user/UserResponse.java
@@ -0,0 +1,22 @@
+package com.lib.ailibrary.user;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserResponse {
+
+    private int userStuId;      // 학번 PK
+    private String userName;    // 학생 이름
+    private String userMajor;   // 학과
+    private String userEmail;   // 학생 이메일
+    private String userPnum;    // 학생 전화번호
+    private String userId;      // 로그인 아이디
+    private String userPw;      // 로그인 비밀번호
+
+}
+
+
