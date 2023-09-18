300baf8d993f45a75e9f4a92777f983d45acf0dc
Junghwan <sl8792@naver.com>
2023-09-18 12:10:27 +0900

    test: JWT Test 성공

--- src/main/java/com/lib/ailibrary/config/SecurityConfig.java
+++ src/main/java/com/lib/ailibrary/config/SecurityConfig.java
@@ -1,12 +1,47 @@
 package com.lib.ailibrary.config;
 
+import com.lib.ailibrary.jwt.JwtAccessDeniedHandler;
+import com.lib.ailibrary.jwt.JwtAuthenticationEntryPoint;
+import com.lib.ailibrary.jwt.JwtSecurityConfig;
+import com.lib.ailibrary.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login").permitAll()
+
+                        .requestMatchers("/api/authenticate").permitAll() // 로그인 api
+                        .requestMatchers("/api/signup").permitAll() // 회원가입 api
+                .anyRequest().authenticated())
+                .csrf(setting -> setting.disable())
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(setting -> setting.disable())
+                .exceptionHandling(authenticationManager -> authenticationManager
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler))
+                .apply(new JwtSecurityConfig(tokenProvider));
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
